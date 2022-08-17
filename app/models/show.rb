45e848d2c110da414924476f04ec222eed4d4808
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-08-17 13:10:55 -0700

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        list_actors = []
+        self.actors.each do |actor|
+            list_actors << "#{actor.first_name} #{actor.last_name}"
+        end
+        list_actors
+    end
 end
